9be72025f92f311427b1e3f69f2494c63f61e8bf
unknown <rosiek.kuba@gmail.com>
2017-07-21 21:14:59 +0100

    laczy imiona

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+var allNames = [];
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var arr = allNames.concat(femaleNames,maleNames);
+var newName = 'Marian'
+	if (allNames.indexOf(newName) === -1) {
+    		allNames.push('newName'); 
+	};
+console.log(arr);
