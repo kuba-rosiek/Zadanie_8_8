1e6e2b3c944a3e9d200a11d683da96d5bc53fe78
kuba-rosiek <rosiek.kuba@gmail.com>
2017-07-24 12:19:43 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // scripts.js
-var allNames = [];
+var arr = [];
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var arr = allNames.concat(femaleNames,maleNames);
+var allNames = arr.concat(femaleNames,maleNames);
 var newName = 'Marian'
 	if (allNames.indexOf(newName) === -1) {
-    	allNames.push('newName'); 
+    	allNames.push(newName); 
 	};
 console.log(allNames);
